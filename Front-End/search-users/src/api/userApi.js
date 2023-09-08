658024a9d23e22dd7397760979396fa487a3e810
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-09-08 18:26:19 -0300

    azure implementation

--- Front-End/search-users/src/api/userApi.js
+++ Front-End/search-users/src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = `http://localhost:3001/api/`
+const BASE_URL = `https://torre-test-adrian.azurewebsites.net/api/`
 
 const getPreviewUsersRequest = async (searchTerm) => {
     try {
